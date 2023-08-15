9ccf826fbed0a5a617179b91f9b98958957c7789
Sidharth Shukla <swayam1044@gmail.com>
2023-08-15 12:27:00 +0530

    basic multer code

--- node/day9/app.js
+++ node/day9/app.js
@@ -4,7 +4,24 @@ const users = require("./MOCK_DATA.json")
 const app = express();
 
 app.get('/users', (req, res)=>{
+    const html = `
+    <ul>
+     ${users.map((user)=> `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `
+    res.send(html)
+})
+
+
+
+app.get('/api/users', (req, res)=>{
     return res.json(users)
+})
+
+app.get("/api/users/:id", (req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=> user.id === id);
+    return res.json(user);
 })
 
 app.listen(3000, ()=> console.log("server running"))
