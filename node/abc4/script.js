888466cb3898a1fd91d7d50b9dc27a02dffc0abf
Sidharth Shukla <swayam1044@gmail.com>
2023-08-22 00:34:11 +0530

    fs learing

---
+++ node/abc4/script.js
@@ -0,0 +1,20 @@
+// const fs = require("fs");
+
+// fs.writeFileSync("file.txt", "This is new file");
+// console.log("file created");
+
+// const filedata = fs.readFileSync("file.txt", "utf-8");
+// console.log(filedata);
+
+// fs.appendFileSync("file.txt", "  again");
+// console.log("file updated");
+
+// fs.unlinkSync("file.txt");
+// console.log("file deleted");
+// fs.renameSync("script.js", "sc.js");
+
+// fs.mkdirSync("abc");
+// const r = fs.readdirSync("abc");
+// console.log(r);
+// fs.renameSync("abc", "def");
+// fs.rmdirSync("def");
